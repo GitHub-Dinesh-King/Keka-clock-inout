3bac8b3fe597156cdbadf435ea68cce1f86da61d
GitHub-Dinesh-King <137073188+GitHub-Dinesh-King@users.noreply.github.com>
2025-05-02 20:03:29 +0300

    Update clockout.py

--- clockout.py
+++ clockout.py
@@ -1,16 +1,44 @@
 import requests
 import datetime
+import os
 
-def get_token():
-    with open('token.txt', 'r') as f:
-        return f.read().strip()
+# Function to call Secret_keys
+def get_keka_token():
+    return os.getenv('KEKA_TOKEN')
+
+def get_telegram_bot_token():
+    return os.getenv('TELEGRAM_BOT_TOKEN')
+
+def get_telegram_chat_id():
+    return os.getenv('TELEGRAM_CHAT_ID')
+    
+# Function to send Telegram notification
+def send_telegram_notification(message):
+    
+    telegram_bot_token = get_telegram_bot_token()
+    telegram_chat_id = get_telegram_chat_id()
+    
+    url = f'https://api.telegram.org/bot{telegram_bot_token}/sendMessage'
+    payload = {
+        'chat_id': telegram_chat_id,
+        'text': message
+    }
+
+    response = requests.post(url, data=payload)
+
+    if response.status_code == 200:
+        print(f"Notification sent: {message}")
+    else:
+        print(f"Failed to send notification. Status code: {response.status_code}")
+
+
 
 def main():
-    token = get_token()
+    keka_token = get_keka_token()
 
     url = "https://maveric.keka.com/k/attendance/api/mytime/attendance/webclockin"
     headers = {
-        "Authorization": f"Bearer {token}",
+        "Authorization": f"Bearer {keka_token}",
         "Content-Type": "application/json",
         "Accept": "application/json",
         "User-Agent": "Mozilla/5.0"
@@ -27,9 +55,18 @@ def main():
     }
 
     response = requests.post(url, headers=headers, json=payload)
-    print(f"Status code: {response.status_code}")
-    print(f"Response Text: Clocked Out")
+    print(f"Clocked Status code: {response.status_code}")
+    #print(f"Response Text: Clocked In")
+    
+    # Check the response status and send Telegram notification
+    if response.status_code == 200:
+        print("Clock-out successful!")
+        send_telegram_notification('✅ Clock-out successful!')  # Send success notification
+    else:
+        print(f"Clock-out failed!")
+        send_telegram_notification('❌ Clock-out failed!')  # Send failure notification
+
+
 
 if __name__ == "__main__":
     main()
- 
