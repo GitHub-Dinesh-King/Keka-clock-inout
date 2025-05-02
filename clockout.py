5259be491f0ab80b2c59884745d6aa37f9cde13f
GitHub-Dinesh-King <137073188+GitHub-Dinesh-King@users.noreply.github.com>
2025-05-02 17:34:23 +0300

    Update clockout.py

--- clockout.py
+++ clockout.py
@@ -1,41 +1,35 @@
 import requests
 import datetime
-import os
 
-# Read token from file -- Old usage
- with open('token.txt', 'r') as f:
-    bearer_token = f.read().strip()
+def get_token():
+    with open('token.txt', 'r') as f:
+        return f.read().strip()
 
-# bearer_token = os.getenv("KEKA_TOKEN")
+def main():
+    token = get_token()
 
-# Set the URL
-url = "https://maveric.keka.com/k/attendance/api/mytime/attendance/webclockin"
+    url = "https://maveric.keka.com/k/attendance/api/mytime/attendance/webclockin"
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        "User-Agent": "Mozilla/5.0"
+    }
 
-# Get current UTC time
-current_time = datetime.datetime.now(datetime.timezone.utc).isoformat()
+    current_time = datetime.datetime.now(datetime.timezone.utc).isoformat()
+    payload = {
+        "timestamp": current_time,
+        "attendanceLogSource": 1,
+        "locationAddress": None,
+        "manualClockinType": 1,
+        "note": "",
+        "originalPunchStatus": 1
+    }
 
-# Set headers
-headers = {
-    "Authorization": f"Bearer {bearer_token}",
-    "Content-Type": "application/json",
-    "Accept": "application/json",
-    "User-Agent": "Mozilla/5.0"
-}
+    response = requests.post(url, headers=headers, json=payload)
+    print(f"Status code: {response.status_code}")
+    print(f"Response Text: Clocked Out")
 
-# Set payload
-payload = {
-    "timestamp": current_time,
-    "attendanceLogSource": 1,
-    "locationAddress": None,
-    "manualClockinType": 1,
-    "note": "",
-    "originalPunchStatus": 1
-}
-
-# Send the POST request
-response = requests.post(url, headers=headers, json=payload)
-
-# Print the response
-print("Status Code:", response.status_code)
-print("Clocked out")
-# print("Response Text:", response.text)
+if __name__ == "__main__":
+    main()
+ 
