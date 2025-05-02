784cb6baf6ab928932e1de5cfdf3ad41367d9e2d
GitHub-Dinesh-King <137073188+GitHub-Dinesh-King@users.noreply.github.com>
2025-05-02 19:15:19 +0300

    Update clockin.py

--- clockin.py
+++ clockin.py
@@ -5,6 +5,25 @@ def get_token():
     with open('token.txt', 'r') as f:
         return f.read().strip()
 
+# Function to send Telegram notification
+def send_telegram_notification(message):
+    bot_token = '7617784862:AAEFIF4eqIWcySITc_DPLNjvcUmll01A_is'  # Telegram bot token
+    chat_id = '1332307993'
+    url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
+    payload = {
+        'chat_id': chat_id,
+        'text': message
+    }
+
+    response = requests.post(url, data=payload)
+
+    if response.status_code == 200:
+        print(f"Notification sent: {message}")
+    else:
+        print(f"Failed to send notification. Status code: {response.status_code}")
+
+
+
 def main():
     token = get_token()
 
@@ -27,8 +46,18 @@ def main():
     }
 
     response = requests.post(url, headers=headers, json=payload)
-    print(f"Status code: {response.status_code}")
-    print(f"Response Text: Clocked In")
+    print(f"Clocked Status code: {response.status_code}")
+    #print(f"Response Text: Clocked In")
+    
+    # Check the response status and send Telegram notification
+    if response.status_code == 200:
+        print("Clock-in successful!")
+        send_telegram_notification(':white_check_mark: Clock-in successful!')  # Send success notification
+    else:
+        print(f"Clock-in failed!")
+        send_telegram_notification(':x: Clock-in failed!')  # Send failure notification
+
+
 
 if __name__ == "__main__":
     main()
